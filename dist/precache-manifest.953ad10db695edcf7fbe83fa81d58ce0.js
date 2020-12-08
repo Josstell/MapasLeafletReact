960d3e439107ae12e2b82eaf7de890dcf5b0513e
José Juan Téllez Guzmán <jjtellezg@gmail.com>
2020-12-07 18:49:45 -0600

    test

---
+++ dist/precache-manifest.953ad10db695edcf7fbe83fa81d58ce0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb130771992d7da659b3d8c11704b4b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77a5fab94c9936307693",
+    "url": "/static/css/2.067ccc96.chunk.css"
+  },
+  {
+    "revision": "3abbfbbc438d110a0b3f",
+    "url": "/static/css/main.45836c82.chunk.css"
+  },
+  {
+    "revision": "77a5fab94c9936307693",
+    "url": "/static/js/2.caeec6cd.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.caeec6cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3abbfbbc438d110a0b3f",
+    "url": "/static/js/main.bf4607ae.chunk.js"
+  },
+  {
+    "revision": "152083ca76e666a82d4f",
+    "url": "/static/js/runtime-main.ee88bf88.js"
+  },
+  {
+    "revision": "2ceaf661f7c099123415f94f97e2a0c5",
+    "url": "/static/media/venue_location_icon.2ceaf661.svg"
+  }
+]);
